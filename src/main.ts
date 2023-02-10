8e0211fe611e1f1a15d59dddc0d54f037360d3fd
pedrohbsilva <phbs1235@gmail.com>
2023-02-10 16:01:38 -0300

    fix: atualização do projeto 16

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const configService = app.get(ConfigService);
-  const PORT = configService.get<number>('port') || 3000;
+  const PORT = process.env.PORT || 3000;
   console.log(process.env.DB_HOST);
   app.useGlobalPipes(
     new ValidationPipe({
