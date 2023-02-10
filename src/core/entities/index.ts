ed7707d7547739acad91ace2cf4b0a3c42a17724
pedrohbsilva <phbs1235@gmail.com>
2023-02-10 11:06:03 -0300

    fix: atualização do projeto 12

--- src/core/entities/index.ts
+++
@@ -1,5 +0,0 @@
-export * from 'src/modules/users/entities/user.entity';
-export * from 'src/modules/cities/entities/city.entity';
-export * from 'src/modules/states/entities/state.entity';
-export * from 'src/modules/countries/entities/country.entity';
-export * from './base.entity';
--- src/modules/app/app.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('public')
-export class AppController {}
